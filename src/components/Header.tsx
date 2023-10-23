b90be721ee5b2164784e65d08c85f1b85f83d1be
sangsang-6914 <sangsang6914@gmail.com>
2023-10-23 16:32:02 +0900

    [ADD] Header 컴포넌트 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom';
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="flex justify-center p-4 sticky top-0 border-b border-b-[#f2f2f2] bg-[#fff]">
+      <Link to="/">
+        <img
+          data-v-54e47384=""
+          src="https://assets.cdn.soomgo.com/icons/logo/navigation_logo.svg"
+          alt="숨고 로고"
+          data-testid="main-logo"
+          className="h-[1.625rem]"
+        ></img>
+      </Link>
+    </header>
+  );
 }
 
 export default Header;
